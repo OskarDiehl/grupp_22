41c8c183b63b8c43c9a4dc4c23a58cf4d3d972cf
makj8672 <makj8672@student.su.se>
2020-10-13 16:29:33 +0200

    "Jag skrev en hällsning! /malin"

--- gitTestBror.java
+++ gitTestBror.java
@@ -6,7 +6,7 @@ public class gitTestBror {
 
         System.out.println("Testar Rebase och grejs");
         System.out.println("Mer bajs tack");
-        System.out.println("Tjo bre!")
+        System.out.println("Tjo bre!") //malte
     }
 
 
