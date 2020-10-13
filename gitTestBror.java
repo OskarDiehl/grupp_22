fec6b988c3550ec0db48e06a8b9679c317e5490b
OskarDiehl <osdi7245@git.dsv.su.se>
2020-10-13 15:52:33 +0200

    Höhöhöhöh

--- gitTestBror.java
+++ gitTestBror.java
@@ -2,6 +2,7 @@ public class gitTestBror {
 
     public static void main(String[] args){
         System.out.println("bajs haha");
+        System.out.println("Testa  bajs för faaaan");
     }
     
 }
