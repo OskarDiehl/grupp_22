b4bcb756c68d441b9593d5f1c79c65bfc1d179cd
OskarDiehl <osdi7245@git.dsv.su.se>
2020-10-14 17:09:07 +0200

    Fråga som komment

--- rogue_like/src/main/java/Test.java
+++ rogue_like/src/main/java/Test.java
@@ -7,6 +7,7 @@ public class Test {
         System.out.println("Fungerar det att kompilera och lägga upp i gitten?/Oskar");
     }
 
+    //Vill någon annan testa att pusha någonting så att jag ser att git fortfarande fungerar?/Oskar
 
     //hej
     //tja
