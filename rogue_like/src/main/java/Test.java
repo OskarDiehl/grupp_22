0f330d7c759f2844931455ba30895aa08faa2f9b
OskarDiehl <osdi7245@git.dsv.su.se>
2020-10-14 16:51:45 +0200

    Testar att se så att jag kan commita och sedan kompilera/Oskar

--- rogue_like/src/main/java/Test.java
+++ rogue_like/src/main/java/Test.java
@@ -1,8 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hej");
+
+
+        System.out.println("Fungerar det att kompilera och lägga upp i gitten?/Oskar");
     }
+
 
     //hej
     //tja
