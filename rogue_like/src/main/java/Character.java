2fd658f58a4183a7be9a847a1f94d5ad14f07f0f
makj8672 <makj8672@student.su.se>
2020-10-15 13:42:32 +0200

    Uppdatering av CharacterTest + Character /Malin

--- rogue_like/src/main/java/Character.java
+++ rogue_like/src/main/java/Character.java
@@ -1,2 +1,17 @@
 public class Character {
+    private String name;
+    private Element element;
+
+    public Character(String name, Element element){
+        this.name = name;
+        this.element = element;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Element getElement() {
+        return element;
+    }
 }
--- rogue_like/src/test/java/CharacterTest.java
+++ rogue_like/src/test/java/CharacterTest.java
@@ -1,22 +1,16 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+//import org.junit.Test;    TODO Är det här något som är relevant? /Malin
 
 public class CharacterTest {
-    @Test
-
-    public void checkIfConstructorWorksRight(){
-        Character character = new Character();
-
-    }
-
 
     @Test
     void testConstructor() {
         Element element = new Element();
-        Character character = new Character();
+        Character character = new Character("Malin", element);
+        assertEquals("Malin" + element , character.getName() + character.getElement());
         //"Malin", element
         //Stats ska kanske sättas utifrån element och roll?
-
     }
 }
