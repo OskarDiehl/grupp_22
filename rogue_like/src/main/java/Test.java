7965537f2cdac64b860dc7cb4f06d5efd41c7ff5
makj8672 <makj8672@student.su.se>
2020-10-15 09:32:13 +0200

    Testar ta bort rouge_lie_grupp_22 / Malin

--- rogue_like/src/main/java/Test.java
+++ rogue_like/src/main/java/Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hej");
+        System.out.println("Hej, jag testar");
 
 
         System.out.println("Fungerar det att kompilera och lägga upp i gitten?/Oskar");
