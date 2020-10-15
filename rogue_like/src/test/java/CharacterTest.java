d1b96033183b18878565c7090ed6600d542076af
sada-1999 <sada1999@student.su.se>
2020-10-15 12:17:36 +0200

    Första testet i CharacterTest påbörjat/Sabina

--- rogue_like/src/test/java/CharacterTest.java
+++ rogue_like/src/test/java/CharacterTest.java
@@ -1,5 +1,12 @@
 import org.junit.jupiter.api.Test;
 
 public class CharacterTest {
+    @Test
+    void testConstructor() {
+        Element element = new Element();
+        Character character = new Character();
+        //"Malin", element
+        //Stats ska kanske snarare sättas utifrån element och roll?
 
+    }
 }
