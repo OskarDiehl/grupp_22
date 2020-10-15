d77823be09bdd9d16c3ad5a07a5915e44d3ed63d
OskarDiehl <osdi7245@git.dsv.su.se>
2020-10-15 14:03:21 +0200

    Har lagt till attack i Element med temporär power

--- rogue_like/src/main/java/Element.java
+++ rogue_like/src/main/java/Element.java
@@ -1,7 +1,24 @@
 public class Element {
 
-public boolean isStrong() {
-    return true;
-}
+    public boolean isStrong() {
+        return true;
+    }
+
+    public boolean isWeak() {
+        return true;
+    }
+
+    public int attack() {
+        int power = 5;
+
+        if (isStrong()) {
+            power += 2;
+        } else if (isWeak()) {
+            power -= 2;
+        }
+
+        return power;
+
+    }
 
 }
--- rogue_like/src/test/java/ElementTest.java
+++ rogue_like/src/test/java/ElementTest.java
@@ -3,15 +3,24 @@ import org.junit.jupiter.api.Test;
 
 public class ElementTest {
 
+
     @Test
-    void createInstancOfElement() {
+    void isStrongMethodReturnsTrue() {
         Element e = new Element();
+
+        assertTrue(e.isStrong());
     }
 
     @Test
-    void isStrongMethodIsTrue() {
+    void isWeakMethodReturnsTrue() {
         Element e = new Element();
-        e.isStrong();
+        assertTrue(e.isWeak());
+    }
+
+    @Test
+    void attackAndIsStrongReturnsSeven() {
+        Element e = new Element();
+        assertEquals(7,e.attack());
     }
 
 }
