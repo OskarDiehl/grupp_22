87ded44cdbefbff71db8ed33268bae34d5925a0e
David <dali0842@student.su.se>
2020-10-15 15:39:34 +0200

    Gjort ändringar i Room och RoomTest

--- rogue_like/src/main/java/Room.java
+++ rogue_like/src/main/java/Room.java
@@ -1,12 +1,68 @@
 public class Room {
     String element; //ska ändras till Element klassen sen
+     private final int STANDARD_THRESHOLD = 6;
+     private final int MAX_THRESHOLD = 10;
+     private final int MIN_THRESHOLD = 1;
 
-    public Room(String element){
-        this.element = element;
+     private final int MAX_AMOUNT_OF_ENEMIES = 7;
+     private final int MIN_AMOUNT_OF_ENEMIES = 4;
 
+     private int threshold;
+
+     public Room(){
+        threshold = STANDARD_THRESHOLD;
+        this.element = "Fire";
+    }
+
+    public boolean spawnItem() {
+        return spawnItem(generateRandomNumber(MIN_THRESHOLD, MAX_THRESHOLD));
+    }
+
+    public boolean spawnItem(int num){
+        return spawnItem(num, threshold);
+    }
+
+    private boolean spawnItem(int num, int threshold){
+        if( num >= threshold){
+            return true;
+        }
+        return false;
     }
 
 
 
+    public void setThreshold(int threshold) {
+        if (threshold > MAX_THRESHOLD || threshold < MIN_THRESHOLD) {
+            throw new IllegalArgumentException();
+        } else {
+            this.threshold = threshold;
+        }
+    }
 
+    public int getThreshold() {
+        return threshold;
+    }
+
+    //När ett rum skapas finns det en threshold som avgör hur stor chans det är att man får dropp, ju lägre threshold är, ju större chans är det att man får drop.
+
+
+
+    public void spawnEnemies(){
+         int quantity = generateAmountOfEnemies();
+
+    }
+    public int generateAmountOfEnemies(){
+         return generateRandomNumber(MIN_AMOUNT_OF_ENEMIES, MAX_AMOUNT_OF_ENEMIES);
+     }
+
+
+
+
+
+//Denna nås av test genom andra metoder, men kan inte testas direkt eftersom den är privat.
+//Man bör fråga sig om denna bör vara publik, samt att den har begränsingar (MIN < MAX exempelvis)
+    private int generateRandomNumber(int min, int max) {
+         return (int) ((Math.random() * (max - min)) + min);
+
+    }
 }
--- rogue_like/src/test/java/RoomTest.java
+++ rogue_like/src/test/java/RoomTest.java
@@ -5,11 +5,91 @@ class RoomTest {
 
 
 
-    @Test
-    void testIfCorrectElement(){
 
+// Ska denna testas med tanke på att den är privat?
+ /*   @Test
+    void shouldGenerateNumberBetweenOneAndTen(){
+
+        Room room = new Room();
+        int num = room.genereateRandomNumber(1,10);
+
+        assertTrue( 1 <= num && num <= 10, "num: " + num);
+    }
+
+  */
+
+
+
+    @Test
+    void spawnItemShouldBeTrue() {
+        Room room = new Room();
+        assertEquals(true, room.spawnItem(6));
+    }
+
+    @Test
+    void spawnItemShouldBeFalse(){
+        Room room = new Room();
+        assertEquals(false, room.spawnItem(5));
+    }
+
+    @Test
+    void spawnItemShouldBeTrueWithNewThreshold(){
+        Room room = new Room();
+        room.setThreshold(3);
+        assertEquals(true, room.spawnItem(4));
+    }
+
+    @Test
+    void spawnItemShouldBeFalseWithNewThreshold(){
+        Room room = new Room();
+        room.setThreshold(8);
+        assertEquals(false, room.spawnItem(7));
+    }
+
+
+    @Test
+    void ifThresholdValueChanges(){
+        Room room = new Room();
+        room.setThreshold(4);
+        assertEquals(4, room.getThreshold());
+    }
+
+    @Test
+    void spawnItemShouldBeTrueAfterSetThreshold(){
+        Room room = new Room();
+        room.setThreshold(1);
+        assertTrue(room.spawnItem());
+    }
+
+    @Test
+    void ifArgumentExceptionIsThrownWhenSetThresholdAboveTen() {
+        Room room = new Room();
+        assertThrows(IllegalArgumentException.class, () -> {
+            room.setThreshold(13);
+        });
+    }
+
+    @Test
+    void ifArgumentExceptionIsThrownWhenSetThresholdBelowOne(){
+        Room room = new Room();
+        assertThrows(IllegalArgumentException.class, () -> {
+            room.setThreshold(-1);
+        });
 
     }
+
+    @Test void ifAllEnemiesAreTheRightElement(){
+
+    }
+
+    @Test void ifCorrectNumberOfEnemiesHaveBeenCreated(){
+
+    }
+
+
+
+
+
 
 
 }
