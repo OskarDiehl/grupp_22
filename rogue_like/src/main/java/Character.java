7f582fdac9a2e9fa657c90d720272375118e6554
sada-1999 <sada1999@student.su.se>
2020-10-19 13:18:50 +0200

    Enemy-konstruktor samt ny bossklass/Sabina

--- rogue_like/src/main/java/Character.java
+++ rogue_like/src/main/java/Character.java
@@ -3,14 +3,14 @@ public class Character {
     // INSTANCE VARIABLES  ------------------------------------------------------------------------
     private String name;
     private Element element;
-    private int level;
+    protected int level;
     protected Stats stats;
 
     // CONSTRUCTOR  ------------------------------------------------------------------------
     public Character(String name, Element element){
         this.name = name;
         this.element = element;
-        this.level = 1;
+        //this.level = 1; TODO Implementera detta i Player Malin /Sabina
     }
 
     // OTHER METHODS ------------------------------------------------------------------------
--- rogue_like/src/main/java/Enemy.java
+++ rogue_like/src/main/java/Enemy.java
@@ -1,6 +1,8 @@
 public class Enemy extends Character {
 
-    public Enemy(String name, Element element) {
+    public Enemy(String name, Element element, int level) {
         super(name, element);
+        this.level = level;
     }
+
 }
