1fa9a48b8fb28af8f3815a87a0ef3ce1d4af33d9
OskarDiehl <osdi7245@git.dsv.su.se>
2020-10-20 12:16:31 +0200

    lade till två metoder i player som är bortkommenterad än så länge

--- rogue_like/src/main/java/Player.java
+++ rogue_like/src/main/java/Player.java
@@ -20,7 +20,7 @@ public class Player extends Character {
     }
 
 
-    @Override
+
     public void levelUpChangeStats(){
         //TODO skapa metoden /Malin
     }
@@ -29,10 +29,20 @@ public class Player extends Character {
 
 
     //Oskar här! Tänker att det kan vara bra att ha en metod som samlar stats från de olika items som spelaren har.
-    //Kommenterar bort metoden nu i alla fall så att den inte stör med det du gör.
+    //Kommenterar bort metoderna nu i alla fall så att den inte stör med det du gör.
     /*public int getItemPower() {
         int itemPower = 7; //7 är en placeholder tills vi kan hämta power från items.
         return itemPower;
+    }
+
+    public int getItemSpeed() {
+        int itemSpeed = 5; //Placeholder
+        return itemSpeed;
+    }
+
+    public int getItemHP() {
+        int itemHP = 3;
+        return itemHP;
     }*/
 
 }
