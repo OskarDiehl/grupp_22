8391b79cc7053f9f7fa27fb753a4bd81af8c0b18
sada-1999 <sada1999@student.su.se>
2020-11-01 15:39:08 +0100

    Ändringar i tester

--- rogue_like/src/test/java/BossTest.java
+++ rogue_like/src/test/java/BossTest.java
@@ -1,23 +1,22 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BossTest {
 
     @Test
-    void generateBossStatsForLevelOne() { //Själva statsen ska ändras
+    void generateBossStatsForLevelOne() {
         Boss boss = new Boss(new FireElement(3), 1, new Room(new Player("Test", new FireElement(1), Role.Tank)));
         assertEquals(new Stats(5, 4, 4).toString(), boss.getStats().toString());
     }
 
     @Test
-    void generateBossStatsForLevelTwo() { //Själva statsen ska ändras
+    void generateBossStatsForLevelTwo() {
         Boss boss = new Boss(new FireElement(3), 2, new Room(new Player("Test", new FireElement(1), Role.Tank)));
         assertEquals(new Stats(7, 6, 6).toString(), boss.getStats().toString());
     }
 
     @Test
-    void generateBossStatsForLevelThree() { //Själva statsen ska ändras
+    void generateBossStatsForLevelThree() {
         Boss boss = new Boss(new FireElement(3), 3, new Room(new Player("Test", new FireElement(1), Role.Tank)));
         assertEquals(new Stats(10, 9, 9).toString(), boss.getStats().toString());
     }
